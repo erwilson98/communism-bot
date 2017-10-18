65d6baf85ab01ea4f0e562fc563e19cb2e7752f6
erwilson98 <32907881+erwilson98@users.noreply.github.com>
2017-10-18 14:03:32 -0400

    the bot stuff

---
+++ config.js
@@ -0,0 +1,8 @@
+//config.js
+
+module.exports = {
+    consumer_key: 'lyZaOE2h4fENvKB6vf6VqBAYi',
+    consumer_secret: 'rSbewQhvbksjeTnmTmMjaFLSOeB1A8dusjOo49t80FZ4HqpiQL',
+    access_token_key: '920679285911883776-Eq0NtlS7oso9UCEIVZhmDeDIu05C0Ag',
+    access_token_secret: 'sYXCDUQiI90wuTYuzyHS3szXb9bCwmLgNqbB7ZFyxFklY'
+}
